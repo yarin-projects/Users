db47d8efdcda72289970210223b56baaa54a8be1
yarin <yarin5445@gmail.com>
2025-03-11 14:02:26 +0200

    Add Color Options For Button Component

--- Frontend/src/components/button.tsx
+++ Frontend/src/components/button.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
+type colorKeys = 'blue' | 'red';
 type ButtonProps = {
   onClick?: () => void;
   children: React.ReactNode;
   type?: 'submit' | 'button';
+  color?: colorKeys;
 };
 
-const Button: React.FC<ButtonProps> = ({ onClick, children, type = 'button' }) => {
+const colorClasses: Record<colorKeys, string> = {
+  blue: 'bg-blue-500',
+  red: 'bg-red-500',
+};
+
+const Button: React.FC<ButtonProps> = ({ onClick, children, type = 'button', color = 'blue' }) => {
   return (
-    <button className="bg-blue-500 text-white p-2 rounded w-full" onClick={onClick} type={type}>
+    <button
+      className={`${colorClasses[color]} text-white p-2 rounded w-full cursor-pointer`}
+      onClick={onClick}
+      type={type}
+    >
       {children}
     </button>
   );
