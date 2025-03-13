b15ebe742dd7778f3b822a0bff4e3c059fe451c1
yarin <yarin5445@gmail.com>
2025-03-13 10:00:37 +0200

    Add Login Test Cases, Additional Sign-Up Case

--- Backend/src/controllers/__test__/user.controller.test.ts
+++ Backend/src/controllers/__test__/user.controller.test.ts
@@ -10,6 +10,36 @@ describe('Tests For User Controller', () => {
         name: 'test',
       });
       expect(respnse.status).toBe(201);
+      expect(respnse.body.token).toBeDefined();
+    });
+    it("Should return status code 400 if name isn't probided", async () => {
+      const respnse = await request(app).post('/api/users/signup').send({
+        email: 'test@test.com',
+        password: '123123',
+      });
+      expect(respnse.status).toBe(400);
+    });
+  });
+  describe('Login', () => {
+    it('Should return status code 200 if logged in successfully', async () => {
+      await request(app).post('/api/users/signup').send({
+        email: 'test@test.com',
+        password: '123123',
+        name: 'test',
+      });
+      const respnse = await request(app).post('/api/users/login').send({
+        email: 'test@test.com',
+        password: '123123',
+      });
+      expect(respnse.status).toBe(200);
+      expect(respnse.body.token).toBeDefined();
+    });
+    it("Should return status code 400 if user doesn't exist", async () => {
+      const respnse = await request(app).post('/api/users/login').send({
+        email: 'test@test.com',
+        password: '123123',
+      });
+      expect(respnse.status).toBe(400);
     });
   });
 });
